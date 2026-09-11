bf557752a434f4007671ea0711ba066b25ca3bd7
rhannink <1661824+rhannink@users.noreply.github.com>
2026-09-11 10:14:20 +0200

    Use translation keys for sensors

--- custom_components/dru_fireplace/sensor.py
+++ custom_components/dru_fireplace/sensor.py
@@ -12,16 +12,16 @@ class Desc(SensorEntityDescription):
 
 
 SENSORS = (
-    Desc(key="hw_type", name="Hardware type", value_fn=lambda d: d.hw_type),
-    Desc(key="sw_version", name="Software version", value_fn=lambda d: d.sw_version),
-    Desc(key="rf_last_seen", name="RF last seen", native_unit_of_measurement="s", value_fn=lambda d: d.rf_last_seen),
-    Desc(key="rf_status", name="RF communication status", value_fn=lambda d: d.rf_status),
-    Desc(key="error_code", name="Error code", value_fn=lambda d: d.error_code),
-    Desc(key="gateway_rssi", name="Gateway RSSI", device_class=SensorDeviceClass.SIGNAL_STRENGTH, native_unit_of_measurement="dBm", value_fn=lambda d: d.gateway_rssi),
-    Desc(key="dfgt_rssi", name="DFGT RSSI", device_class=SensorDeviceClass.SIGNAL_STRENGTH, native_unit_of_measurement="dBm", value_fn=lambda d: d.dfgt_rssi),
-    Desc(key="room_temperature", name="Room temperature", device_class=SensorDeviceClass.TEMPERATURE, native_unit_of_measurement=UnitOfTemperature.CELSIUS, value_fn=lambda d: d.room_temperature),
-    Desc(key="temperature_setpoint", name="Temperature setpoint", device_class=SensorDeviceClass.TEMPERATURE, native_unit_of_measurement=UnitOfTemperature.CELSIUS, value_fn=lambda d: d.temperature_setpoint),
-    Desc(key="temperature_control_state", name="Temperature control state", value_fn=lambda d: ("not_possible", "possible", "active", "error")[d.temperature_control_state]),
+    Desc(key="hw_type", translation_key="hw_type", value_fn=lambda d: d.hw_type),
+    Desc(key="sw_version", translation_key="sw_version", value_fn=lambda d: d.sw_version),
+    Desc(key="rf_last_seen", translation_key="rf_last_seen", native_unit_of_measurement="s", value_fn=lambda d: d.rf_last_seen),
+    Desc(key="rf_status", translation_key="rf_status", value_fn=lambda d: d.rf_status),
+    Desc(key="error_code", translation_key="error_code", value_fn=lambda d: d.error_code),
+    Desc(key="gateway_rssi", translation_key="gateway_rssi", device_class=SensorDeviceClass.SIGNAL_STRENGTH, native_unit_of_measurement="dBm", value_fn=lambda d: d.gateway_rssi),
+    Desc(key="dfgt_rssi", translation_key="dfgt_rssi", device_class=SensorDeviceClass.SIGNAL_STRENGTH, native_unit_of_measurement="dBm", value_fn=lambda d: d.dfgt_rssi),
+    Desc(key="room_temperature", translation_key="room_temperature", device_class=SensorDeviceClass.TEMPERATURE, native_unit_of_measurement=UnitOfTemperature.CELSIUS, value_fn=lambda d: d.room_temperature),
+    Desc(key="temperature_setpoint", translation_key="temperature_setpoint", device_class=SensorDeviceClass.TEMPERATURE, native_unit_of_measurement=UnitOfTemperature.CELSIUS, value_fn=lambda d: d.temperature_setpoint),
+    Desc(key="temperature_control_state", translation_key="temperature_control_state", value_fn=lambda d: ("not_possible", "possible", "active", "error")[d.temperature_control_state]),
 )
 
 
