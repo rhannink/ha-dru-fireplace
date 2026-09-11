ed43de3cba28bf65b32ea971062537ee56b83ca9
rhannink <1661824+rhannink@users.noreply.github.com>
2026-09-11 10:28:41 +0200

    Remove unsupported temperature control switch

--- custom_components/dru_fireplace/switch.py
+++ custom_components/dru_fireplace/switch.py
@@ -6,17 +6,15 @@ from .entity import DruEntity
 
 @dataclass(frozen=True, kw_only=True)
 class Desc(SwitchEntityDescription):
-    bit: int | None
+    bit: int
     on_action: int
     off_action: int
-    temp_control: bool = False
 
 
 SWITCHES = (
     Desc(key="main_burner", translation_key="main_burner", bit=STATUS_MAIN_BURNER, on_action=ACTION_MAIN_BURNER_ON, off_action=ACTION_MAIN_BURNER_OFF),
     Desc(key="second_burner", translation_key="second_burner", bit=STATUS_SECOND_BURNER, on_action=ACTION_SECOND_BURNER_ON, off_action=ACTION_SECOND_BURNER_OFF),
     Desc(key="wave", translation_key="wave", bit=STATUS_WAVE, on_action=ACTION_WAVE_ON, off_action=ACTION_WAVE_OFF),
-    Desc(key="temperature_control", translation_key="temperature_control", bit=None, on_action=ACTION_TEMP_CONTROL_ON, off_action=ACTION_TEMP_CONTROL_OFF, temp_control=True),
 )
 
 
@@ -28,8 +26,6 @@ class DruSwitch(DruEntity, SwitchEntity):
 
     @property
     def is_on(self):
-        if self.entity_description.temp_control:
-            return self.coordinator.data.temperature_control_state == 2
         return self.coordinator.data.status_bit(self.entity_description.bit)
 
     async def async_turn_on(self, **kwargs):
